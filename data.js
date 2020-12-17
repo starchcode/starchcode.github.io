0e275da510a6b01f069e977243e281e145e29d9b
Starch <mrdave67@gmail.com>
2020-12-17 12:48:19 +0000

    work page updated

---
+++ data.js
@@ -0,0 +1,31 @@
+const work =[
+    {
+        'title': 'CashewTree.ie',
+        'img': 'files/cashewscrshot.png',
+        'short-desc': 'a website for a food brand based in Dublin, Ireland',
+        'date': 'October 20',
+        'description': 'Dynamic single page website. Created with goals such as giving the ability to the client to update products page as well as FAQs. started with wireframing and getting agreement about UIs from the client. Followed by design from scratch using Affinity designer and finally coding and deployment on GoDaddy hosting services.',
+        'tech': [
+            'HTML & CSS','Use of Wordpress as headless CMS so the client can interact with the website. Website is dynamic thanks to  the Wordpress API.'
+            , 'jQuery for handling animations and JavaScript to handle menu animations and more.'
+        ],
+        'project-link': 'http://cashewtree.ie/',
+        'github': ['https://github.com/starchcode/cashewtree.ie']
+    },
+    {
+        'title': 'theGingerBlondieBakery.ie',
+        'img': './files/gingerscrshot.png',
+        'short-desc': ' a website for a food brand based in Dublin, Ireland',
+        'date': 'November 2020 - Present',
+        'description': 'A similar project for a food brand. However I challenged myself using a freshly learned framework (ReactJs) to build a dynamic website. Goals included contact from, newsletter, blog and products page. Here I had to create a backend app that can handle all the API calls safely without exposing sensitive data. Website is to be deployed on a VPS such as DigitalOcean to challenge myself learning new skills as well as keeping the costs down so client and server side can be launched on a single low cost VPS. The project will be delivered before the new year. Here is a video of current progress. Also codes are being pushed on my github repository as I am working on it.',
+        'tech': [ 'HTML & CSS',
+            'JavaScript ReactJs',
+            'Wordpress as headless CMS',
+            'NodeJs (expressJS): use of nodemailer for contact form to keep the costs down since it is not a high traffic website at the moment.'
+        ],
+        'project-link': '#',
+        'github': ['https://github.com/starchcode/gingerblondiebakery', 'https://github.com/starchcode/gingerblondiebackend'],
+        'youtube': '<iframe width="560" height="315" src="https://www.youtube.com/embed/VUemFXpmCDU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+    }
+]
+
--- script.js
+++ script.js
@@ -1,93 +1,84 @@
-const underNav = document.getElementById('underNav')
-const pages = document.querySelectorAll('.main');
-const menuButtons = document.querySelectorAll('.menuBut');
-const underNavPos = ['0', '25%', '50%', '75%'];
-
-
+const underNav = document.getElementById("underNav");
+const pages = document.querySelectorAll(".main");
+const menuButtons = document.querySelectorAll(".menuBut");
+const underNavPos = ["0", "25%", "50%", "75%"];
 
 //Navigation: animation settings
-const navAnimDur = 0.25
+const navAnimDur = 0.25;
 // const navAnim = navAnimDur.toString() + 's ease-out 0s 1 normal forwards running ';
 
 function isVisible(element) {
   let elementBox = element.getBoundingClientRect();
-    if(elementBox.top < window.innerHeight / 3 ){
-      return true;
+  if (elementBox.top < window.innerHeight / 3) {
+    return true;
   } else {
-      return false;
-  } 
+    return false;
+  }
 }
 function isVisibleContent(element) {
   let elementBox = element.getBoundingClientRect();
-    if(elementBox.top < window.innerHeight * 3 / 4 ){
-      return true;
+  if (elementBox.top < (window.innerHeight * 3) / 4) {
+    return true;
   } else {
-      return false;
+    return false;
   }
 }
 function scanDocument() {
-  pages.forEach(function(page, index) {
-     if (isVisible(page)){
-        underNav.style.left = underNavPos[index];
+  pages.forEach(function (page, index) {
+    if (isVisible(page)) {
+      underNav.style.left = underNavPos[index];
 
-        for(i = 0; i<menuButtons.length; i++){
-          i == index? menuButtons[index].style.color = '#A04000': menuButtons[i].style.color = 'initial'
-        }
-        
-     }
-   });
-   
-     let sectionList = document.querySelectorAll('.leftAnim');
-     sectionList.forEach(function(section, index) {
-         if(isVisibleContent(section)) {
- 
-      section.classList.remove('leftAnim');
-         };
-     });
- 
- }
- 
+      for (i = 0; i < menuButtons.length; i++) {
+        i == index
+          ? (menuButtons[index].style.color = "#A04000")
+          : (menuButtons[i].style.color = "initial");
+      }
+    }
+  });
+
+  let sectionList = document.querySelectorAll(".leftAnim");
+  sectionList.forEach(function (section, index) {
+    if (isVisibleContent(section)) {
+      section.classList.remove("leftAnim");
+    }
+  });
+}
+
 document.addEventListener("scroll", _.throttle(scanDocument, 500));
 
-
-
-
 //email copy func.
-const emailAdd = document.getElementById('emailAdd');
-const emailAdd2 = document.getElementById('emailAdd2');
+const emailAdd = document.getElementById("emailAdd");
+const emailAdd2 = document.getElementById("emailAdd2");
 function emailCopier() {
-    // source: https://hackernoon.com/copying-text-to-clipboard-with-javascript-df4d4988697f
-    const el = document.createElement('textarea');  // Create a <textarea> element
-    el.value = "starchCode@gmail.com";                  // Set its value to the string that you want copied
-    el.setAttribute('readonly', '');                // Make it readonly to be tamper-proof
-    el.style.position = 'absolute';                 
-    el.style.left = '-9999px';                      // Move outside the screen to make it invisible
-    document.body.appendChild(el);                  // Append the <textarea> element to the HTML document
-    const selected =            
-      document.getSelection().rangeCount > 0        // Check if there is any content selected previously
-        ? document.getSelection().getRangeAt(0)     // Store selection if found
-        : false;                                    // Mark as false to know no selection existed before
-    el.select();                                    // Select the <textarea> content
-    document.execCommand('copy');                   // Copy - only works as a result of a user action (e.g. click events)
-    document.body.removeChild(el);                  // Remove the <textarea> element
-    /*if (selected) {                                 // If a selection existed before copying
+  // source: https://hackernoon.com/copying-text-to-clipboard-with-javascript-df4d4988697f
+  const el = document.createElement("textarea"); // Create a <textarea> element
+  el.value = "starchCode@gmail.com"; // Set its value to the string that you want copied
+  el.setAttribute("readonly", ""); // Make it readonly to be tamper-proof
+  el.style.position = "absolute";
+  el.style.left = "-9999px"; // Move outside the screen to make it invisible
+  document.body.appendChild(el); // Append the <textarea> element to the HTML document
+  const selected =
+    document.getSelection().rangeCount > 0 // Check if there is any content selected previously
+      ? document.getSelection().getRangeAt(0) // Store selection if found
+      : false; // Mark as false to know no selection existed before
+  el.select(); // Select the <textarea> content
+  document.execCommand("copy"); // Copy - only works as a result of a user action (e.g. click events)
+  document.body.removeChild(el); // Remove the <textarea> element
+  /*if (selected) {                                 // If a selection existed before copying
       document.getSelection().removeAllRanges();    // Unselect everything on the HTML document
       document.getSelection().addRange(selected);   // Restore the original selection
     }*/
-    document.getElementById('copied').style.visibility = "visible";
-    setTimeout(() => { document.getElementById('copied').style.visibility = "hidden";
+  document.getElementById("copied").style.visibility = "visible";
+  setTimeout(() => {
+    document.getElementById("copied").style.visibility = "hidden";
   }, 3000);
-};
-
-
-
-
-
+}
 
 // change content based on query
 function myFunction(x) {
-  if (x.matches) { // If media query matches
-    document.getElementById("paragraphContact").style.display = "none"
+  if (x.matches) {
+    // If media query matches
+    document.getElementById("paragraphContact").style.display = "none";
     emailAdd.onmousedown = null;
     emailAdd2.onmousedown = null;
   } else {
@@ -97,9 +88,45 @@ function myFunction(x) {
   }
 }
 
-var x = window.matchMedia("(min-width: 375px) and (max-width: 667px)")
-myFunction(x) // Call listener function at run time
-x.addListener(myFunction) // Attach listener function on state changes
+var x = window.matchMedia("(min-width: 375px) and (max-width: 667px)");
+myFunction(x); // Call listener function at run time
+x.addListener(myFunction); // Attach listener function on state changes
+
+
+
+function listTech(project){
+let result = '';
+  for(let i=0; i< project.tech.length; i++){
+    result += `<li>${project.tech[i]}</li>`
+  }
+  return result
+}
+function listGit(project){
+  let result = '';
+    for(let i=0; i< project.github.length; i++){
+      result += `<a href='${project.github[i]}' target='_blank'>${project.github[i]}</a>`
+    }
+    return result
+  }
+work.forEach((div) => {
+  $("#myWork").append(`
+<div class='MyWorkContent'>
+<a href='${div["project-link"]}' target='_blank' ><h1>${div.title}</h1>
+<div class='imgCover'><img width='100%' src=${div.img} /></div></a>
+<p>${div.date}</p>
+<h3>${div["short-desc"]}</h3>
+<p>${div.description}</p>
+<h4>Technology used:</h4>
+<ul id='myWorkTech'>
+${listTech(div)}
+</ul>
+<h4>code on github:</h4>
+<ul>
+${listGit(div)}</ul>
+${div.youtube? div.youtube: ''}
+</div>
+`);
+});
 
 /*useful code for window y offset
 https://javascript.info/onscroll
