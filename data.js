fc093208ae2f03c3214de01673f0bab8589d21c7
Starch <mrdave67@gmail.com>
2020-12-24 13:41:47 +0000

    mywork updated

--- data.js
+++ data.js
@@ -16,16 +16,17 @@ const work =[
         'title': 'theGingerBlondieBakery.ie',
         'img': './files/gingerscrshot.png',
         'short-desc': ' a website for a food brand based in Dublin, Ireland',
-        'date': 'November 2020 - Present',
-        'description': 'A similar project for a food brand. However I challenged myself using a freshly learned framework (ReactJs) to build a dynamic website. Goals included contact from, newsletter, blog and products page. Here I had to create a backend app that can handle all the API calls safely without exposing sensitive data. Website is to be deployed on a VPS such as DigitalOcean to challenge myself learning new skills as well as keeping the costs down so client and server side can be launched on a single low cost VPS. The project will be delivered before the new year. Here is a video of current progress. Also codes are being pushed on my github repository as I am working on it.',
+        'date': 'December 2020',
+        'description': 'A project for a food brand. However I used ReactJs to build a dynamic website. Goals included contact from, newsletter, blog and products page. Here I had to create a backend app that can handle all the API calls safely without exposing sensitive data. Website is deployed on a VPS to challenge myself and learn new skills (LEMP stack). Here you can have a look at my codes on github',
         'tech': [ 'HTML & CSS',
             'JavaScript ReactJs',
             'Wordpress as headless CMS',
-            'NodeJs (expressJS): use of nodemailer for contact form to keep the costs down since it is not a high traffic website at the moment.'
+            'NodeJs (expressJS): use of nodemailer for contact form to keep the costs down since it is not a high traffic website at the moment.',
+            'LEMP stack'
         ],
-        'project-link': '#',
+        'project-link': 'http://thegingerblondie.ie/',
         'github': ['https://github.com/starchcode/gingerblondiebakery', 'https://github.com/starchcode/gingerblondiebackend'],
-        'youtube': '<iframe width="90%" height="250px" src="https://www.youtube.com/embed/VUemFXpmCDU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        // 'youtube': '<iframe width="90%" height="250px" src="https://www.youtube.com/embed/VUemFXpmCDU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
     }
 ]
 
