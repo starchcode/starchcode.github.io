90e15ea3a7c53d5192544bdd35b2f1ee1cbb7d25
Starch <mrdave67@gmail.com>
2020-12-27 11:53:58 +0000

    minor adjustment

--- data.js
+++ data.js
@@ -1,18 +1,5 @@
 const work =[
     {
-        'title': 'CashewTree.ie',
-        'img': 'files/cashewscrshot.png',
-        'short-desc': 'a website for a food brand based in Dublin, Ireland',
-        'date': 'October 20',
-        'description': 'Dynamic single page website. Created with goals such as giving the ability to the client to update products page as well as FAQs. started with wireframing and getting agreement about UIs from the client. Followed by design from scratch using Affinity designer and finally coding and deployment on GoDaddy hosting services.',
-        'tech': [
-            'HTML & CSS','Use of Wordpress as headless CMS so the client can interact with the website. Website is dynamic thanks to  the Wordpress API.'
-            , 'jQuery for handling animations and JavaScript to handle menu animations and more.'
-        ],
-        'project-link': 'http://cashewtree.ie/',
-        'github': ['https://github.com/starchcode/cashewtree.ie']
-    },
-    {
         'title': 'theGingerBlondieBakery.ie',
         'img': './files/gingerscrshot.png',
         'short-desc': ' a website for a food brand based in Dublin, Ireland',
@@ -27,6 +14,19 @@ const work =[
         'project-link': 'http://thegingerblondie.ie/',
         'github': ['https://github.com/starchcode/gingerblondiebakery', 'https://github.com/starchcode/gingerblondiebackend'],
         // 'youtube': '<iframe width="90%" height="250px" src="https://www.youtube.com/embed/VUemFXpmCDU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+    },
+    {
+        'title': 'CashewTree.ie',
+        'img': 'files/cashewscrshot.png',
+        'short-desc': 'a website for a food brand based in Dublin, Ireland',
+        'date': 'October 20',
+        'description': 'Dynamic single page website. Created with goals such as giving the ability to the client to update products page as well as FAQs. started with wireframing and getting agreement about UIs from the client. Followed by design from scratch using Affinity designer and finally coding and deployment on GoDaddy hosting services.',
+        'tech': [
+            'HTML & CSS','Use of Wordpress as headless CMS so the client can interact with the website. Website is dynamic thanks to  the Wordpress API.'
+            , 'jQuery for handling animations and JavaScript to handle menu animations and more.'
+        ],
+        'project-link': 'http://cashewtree.ie/',
+        'github': ['https://github.com/starchcode/cashewtree.ie']
     }
 ]
 
